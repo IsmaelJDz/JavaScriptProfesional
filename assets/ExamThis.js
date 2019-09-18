2462ff339c49d02c4c937eda3c3315fb8c2cb2ff
Rene Ismael <Ismael_br7@hotmail.com>
2019-09-18 02:34:53 -0500

    Bind, apply and call

--- assets/ExamThis.js
+++ assets/ExamThis.js
@@ -1,14 +1,47 @@
-const persona = {
-  name: "ismael",
-  saludar: function(name) {
-    console.log(`hola ${this.name}`);
-  }
+// const persona = {
+//   name: "ismael",
+//   saludar: function(name) {
+//     console.log(`hola ${this.name}`);
+//   }
+// };
+
+// persona.saludar();
+
+// const saludo = persona.saludar;
+// //No obtiene this porque hace referencia
+// //a la funcion donde no existe en el contexto
+// //name (closures);
+// saludo();
+
+function saludar() {
+  console.log(`Hola. Soy ${this.name} ${this.apellido}`);
+}
+
+const ismael = {
+  name: "Ismael",
+  apellido: "Barrios"
 };
 
-persona.saludar();
+saludar.call(ismael);
 
-const saludo = persona.saludar;
-//No obtiene this porque hace referencia
-//a la funcion donde no existe en el contexto
-//name (closures);
-saludo();
+function caminar(metros, direccion) {
+  console.log(`${this.name} camina ${metros} metros hacia ${direccion}`);
+}
+
+caminar.call(ismael, 400, "norte");
+
+caminar.apply(ismael, [500, "sur"]);
+
+const oscar = {
+  name: "Oscar",
+  apellido: "Sanchez"
+};
+
+const DanielSaluda = saludar.bind(oscar);
+DanielSaluda();
+
+const DanielCamina = caminar.bind(oscar);
+DanielCamina(1000, "Este");
+
+const IsmaelCamina = caminar.bind(oscar, 2000);
+IsmaelCamina("Norte");
